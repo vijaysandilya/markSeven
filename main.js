e9fa5d93ea58e4626d269b374519abe6029187ec
vijaysandilya <bvijaysandilya@gmail.com>
2020-12-20 03:02:06 +0530

    Done MarkSeven

---
+++ main.js
@@ -0,0 +1,26 @@
+var buttonTranslate = document.querySelector("#btn-translate");
+var ipText = document.querySelector("#inputID");
+var outputField = document.querySelector("#output");
+
+var sourceURL = "https://api.funtranslations.com/translate/dothraki.json";
+
+function getTranslationURL(input) 
+{
+    return sourceURL + "?text=" + input;
+}
+
+function clickHandler() 
+{
+    var textIp = ipText.value;
+    
+    fetch(getTranslationURL(textIp))
+        .then(response => response.json())
+        .then(json => 
+            {
+            var translatedText = json.contents.translated;
+            outputField.innerText = translatedText;
+           })
+
+}
+
+buttonTranslate.addEventListener("click",clickHandler);
